2ad0044c3bbf791039714dd5b465498ec6668e39
Clément Vandendaelen <cv@taktik.com>
2022-02-18 11:53:14 +0100

    Added checkTokenValidity and createOrModifyUser

--- apis/impl/UserApiImpl.ts
+++ apis/impl/UserApiImpl.ts
@@ -2,23 +2,56 @@ import {User} from "../../models/User";
 import {Filter} from "../../models/Filter";
 import {PaginatedListUser} from "../../models/PaginatedListUser";
 import {UserApi} from "../UserApi";
+import {
+  IccAuthApi,
+  IccCodeApi,
+  IccContactXApi,
+  IccCryptoXApi,
+  IccDocumentXApi,
+  IccHcpartyXApi,
+  IccHelementXApi,
+  IccPatientXApi,
+  IccUserApi,
+  IccUserXApi
+} from "@icure/api";
+import {IccDeviceApi} from "@icure/api/icc-api/api/IccDeviceApi";
+import {UserMapper} from "../../mappers/user";
 
 class UserApiImpl implements UserApi {
-  checkTokenValidity(userId: string, token: string): Promise<boolean> {
-    return Promise.resolve(false);
+  private userApi: IccUserApi;
+
+  constructor(api: { cryptoApi: IccCryptoXApi; codeApi: IccCodeApi, authApi: IccAuthApi; userApi: IccUserXApi; patientApi: IccPatientXApi; healthcarePartyApi: IccHcpartyXApi; contactApi: IccContactXApi; healthcareElementApi: IccHelementXApi; documentApi: IccDocumentXApi; deviceApi: IccDeviceApi; }) {
+    this.userApi = api.userApi
   }
 
-  createOrModifyUser(user: User): Promise<User> {
-    return Promise.resolve(undefined);
+  async checkTokenValidity(userId: string, token: string): Promise<boolean> {
+    return await this.userApi.checkTokenValidity(userId, token)
   }
 
-    createToken(userId: string): Promise<string> {
-        return Promise.resolve("");
+  async createOrModifyUser(user: User): Promise<User> {
+    const {toUserDto, toUser} = UserMapper;
+    if (user.rev != null) {
+      const createdUser = await this.userApi.createUser(toUserDto(user));
+      if (createdUser != undefined) {
+        return toUser(createdUser)!;
+      }
+      throw new Error("Couldn't create user")
     }
 
-    deleteUser(userId: string): Promise<string> {
-        return Promise.resolve("");
+    const updatedUser = await this.userApi.modifyUser(toUserDto(user));
+    if (updatedUser != undefined) {
+      return toUser(updatedUser)!
     }
+    throw new Error("Couldn't update user")
+  }
+
+  createToken(userId: string): Promise<string> {
+    return Promise.resolve("");
+  }
+
+  deleteUser(userId: string): Promise<string> {
+    return Promise.resolve("");
+  }
 
     filterUsers(filter: Filter, nextUserId?: string, limit?: number): Promise<PaginatedListUser> {
         return Promise.resolve(undefined);
