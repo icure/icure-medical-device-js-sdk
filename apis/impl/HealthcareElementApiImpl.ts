723e4c23fbf94fbea0ca9b14cb98408f8d7d966c
Clément Vandendaelen <cv@taktik.com>
2022-02-21 11:29:48 +0100

    Added HealthElement impl

--- apis/impl/HealthcareElementApiImpl.ts
+++ apis/impl/HealthcareElementApiImpl.ts
@@ -2,29 +2,69 @@ import {HealthcareElement} from "../../models/HealthcareElement";
 import {Filter} from "../../models/Filter";
 import {PaginatedListHealthcareElement} from "../../models/PaginatedListHealthcareElement";
 import {HealthcareElementApi} from "../HealthcareElementApi";
+import {IccAuthApi, IccCodeApi, IccDocumentXApi, IccPatientXApi, IccUserXApi} from "@icure/api";
+import {IccHcpartyXApi} from "@icure/api/icc-x-api/icc-hcparty-x-api";
+import {IccCryptoXApi} from "@icure/api/icc-x-api/icc-crypto-x-api";
+import {IccContactXApi} from "@icure/api/icc-x-api/icc-contact-x-api";
+import {IccHelementXApi} from "@icure/api/icc-x-api/icc-helement-x-api";
+import {HealthcareElementMapper} from "../../dist/mappers/HealthcareElement";
+import {forceUuid} from "../../mappers/utils";
+import {PaginatedListMapper} from "../../mappers/paginatedList";
+import toHealthcareElement = HealthcareElementMapper.toHealthcareElement;
+import toHealthElementDto = HealthcareElementMapper.toHealthElementDto;
 
 class HealthcareElementApiImpl implements HealthcareElementApi {
-  createOrModifyHealthcareElement(healthcareElement: HealthcareElement): Promise<HealthcareElement> {
-    return Promise.resolve(undefined);
+  userApi: IccUserXApi;
+  heApi: IccHelementXApi;
+
+  constructor(api: { cryptoApi: IccCryptoXApi; codeApi: IccCodeApi, authApi: IccAuthApi; userApi: IccUserXApi; patientApi: IccPatientXApi; healthcarePartyApi: IccHcpartyXApi; contactApi: IccContactXApi; healthcareElementApi: IccHelementXApi; documentApi: IccDocumentXApi; }) {
+    this.userApi = api.userApi;
+    this.heApi = api.healthcareElementApi;
   }
 
-  createOrModifyHealthcareElements(healthcareElement: Array<HealthcareElement>): Promise<Array<HealthcareElement>> {
-    return Promise.resolve(undefined);
+  async createOrModifyHealthcareElement(healthcareElement: HealthcareElement): Promise<HealthcareElement> {
+    let createdOrUpdateHealthElement = healthcareElement.rev
+      ? await this.heApi.modifyHealthElement(toHealthElementDto(healthcareElement))
+      : await this.heApi.createHealthElement(toHealthElementDto(healthcareElement))
+
+    if (createdOrUpdateHealthElement) {
+      return toHealthcareElement(createdOrUpdateHealthElement)!;
+    }
+
+    throw Error(`Could not create / modify healthElement ${healthcareElement.id}`)
   }
 
-    deleteHealthcareElement(id: string): Promise<string> {
-        return Promise.resolve("");
+  async createOrModifyHealthcareElements(healthcareElement: Array<HealthcareElement>): Promise<Array<HealthcareElement>> {
+    const heToCreate = healthcareElement.filter(he => he.rev == null)
+    const heToUpdate = healthcareElement.filter(he => he != null)
+
+    if (!heToUpdate.every(he => he.id != null && forceUuid(he.id))) {
+      throw Error("Update id should be provided as an UUID");
     }
 
-    filterHealthcareElement(filter: Filter): Promise<PaginatedListHealthcareElement> {
-        return Promise.resolve(undefined);
-    }
+    const hesCreated = await this.heApi.createHealthElements(heToCreate.map(he => toHealthElementDto(he)))
+    const hesUpdated = await this.heApi.modifyHealthElements(heToUpdate.map(he => toHealthElementDto(he)))
 
-    getHealthcareElement(id: string): Promise<HealthcareElement> {
-        return Promise.resolve(undefined);
-    }
+    return [...hesCreated, ...hesUpdated].map(he => toHealthcareElement(he))
+  }
 
-    matchHealthcareElement(filter: Filter): Promise<Array<string>> {
-        return Promise.resolve(undefined);
+  async deleteHealthcareElement(id: string): Promise<string> {
+    const deletedHeRev = (await this.heApi.deleteHealthElements(id)).find(e => true)?.rev
+    if (deletedHeRev) {
+      return deletedHeRev
     }
+    throw Error(`Could not delete healthcare element ${id}`)
+  }
+
+  filterHealthcareElement(filter: Filter): Promise<PaginatedListHealthcareElement> {
+    return Promise.resolve(new PaginatedListHealthcareElement({}))
+  }
+
+  async getHealthcareElement(id: string): Promise<HealthcareElement> {
+    return toHealthcareElement(await this.heApi.getHealthElement(id))
+  }
+
+  matchHealthcareElement(filter: Filter): Promise<Array<string>> {
+    return Promise.resolve([])
+  }
 }
