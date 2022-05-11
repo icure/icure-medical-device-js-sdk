870875fec3e9ca7c40e521e309c7bc23a17779e0
Lisa Trapletti <lt@icure.io>
2022-05-11 11:00:02 +0200

    IMTS-13 #time 2h Implemented giveAccessTo for PatientApi, related tests and fixed Patient User sign-up

--- src/apis/impl/PatientApiImpl.ts
+++ src/apis/impl/PatientApiImpl.ts
@@ -1,87 +1,98 @@
-import {Patient} from "../../models/Patient";
-import {PaginatedListPatient} from "../../models/PaginatedListPatient";
-import {PatientApi} from "../PatientApi";
+import {Patient} from '../../models/Patient'
+import {PaginatedListPatient} from '../../models/PaginatedListPatient'
+import {PatientApi} from '../PatientApi'
 import {
   FilterChainPatient,
   IccContactXApi,
   IccCryptoXApi,
   IccDocumentXApi,
   IccPatientXApi,
-  IccUserXApi
-} from "@icure/api";
-import {FilterMapper} from "../../mappers/filter";
-import {PaginatedListMapper} from "../../mappers/paginatedList";
-import {Filter} from "../../filter/Filter";
-import {PatientMapper} from "../../mappers/patient";
-import {Connection, ConnectionImpl} from "../../models/Connection";
-import {subscribeToEntityEvents} from "../../utils/rsocket";
+  IccUserXApi,
+  Patient as PatientDto
+} from '@icure/api'
+import {FilterMapper} from '../../mappers/filter'
+import {PaginatedListMapper} from '../../mappers/paginatedList'
+import {Filter} from '../../filter/Filter'
+import {PatientMapper} from '../../mappers/patient'
+import {Connection, ConnectionImpl} from '../../models/Connection'
+import {subscribeToEntityEvents} from '../../utils/rsocket'
 
 export class PatientApiImpl implements PatientApi {
-  private readonly userApi: IccUserXApi;
-  private readonly patientApi: IccPatientXApi;
-  private readonly cryptoApi: IccCryptoXApi;
+  private readonly userApi: IccUserXApi
+  private readonly patientApi: IccPatientXApi
+  private readonly cryptoApi: IccCryptoXApi
 
-  private readonly basePath: string;
-  private readonly username?: string;
-  private readonly password?: string;
+  private readonly basePath: string
+  private readonly username?: string
+  private readonly password?: string
 
-  constructor(api: { cryptoApi: IccCryptoXApi; userApi: IccUserXApi; patientApi: IccPatientXApi; contactApi: IccContactXApi; documentApi: IccDocumentXApi; },
-              basePath: string,
-              username: string | undefined,
-              password: string | undefined) {
-    this.basePath = basePath;
-    this.username = username;
-    this.password = password;
-    this.userApi = api.userApi;
-    this.patientApi = api.patientApi;
-    this.cryptoApi = api.cryptoApi;
+  constructor(
+    api: { cryptoApi: IccCryptoXApi; userApi: IccUserXApi; patientApi: IccPatientXApi; contactApi: IccContactXApi; documentApi: IccDocumentXApi },
+    basePath: string,
+    username: string | undefined,
+    password: string | undefined
+  ) {
+    this.basePath = basePath
+    this.username = username
+    this.password = password
+    this.userApi = api.userApi
+    this.patientApi = api.patientApi
+    this.cryptoApi = api.cryptoApi
   }
 
   async createOrModifyPatient(patient: Patient): Promise<Patient> {
-    let currentUser = await this.userApi.getCurrentUser();
+    let currentUser = await this.userApi.getCurrentUser()
 
     let createdOrUpdatedPatient = patient.rev
       ? await this.patientApi.modifyPatientWithUser(currentUser, PatientMapper.toPatientDto(patient))
       : await this.patientApi.createPatientWithUser(currentUser, await this.patientApi.newInstance(currentUser, PatientMapper.toPatientDto(patient)))
 
     if (createdOrUpdatedPatient) {
-      return PatientMapper.toPatient(createdOrUpdatedPatient)!;
+      return PatientMapper.toPatient(createdOrUpdatedPatient)!
     }
 
     throw Error(`Could not create / modify patient ${patient.id} with user ${currentUser.id}`)
   }
 
   async deletePatient(patientId: string): Promise<string> {
-    let deletedId = (await this.patientApi.deletePatient(patientId))
-      .map((docIdentifier) => docIdentifier.id!)
-      .pop();
+    let deletedId = (await this.patientApi.deletePatient(patientId)).map((docIdentifier) => docIdentifier.id!).pop()
 
     if (deletedId) {
-      return deletedId;
+      return deletedId
     }
 
     throw Error(`Could not delete patient ${patientId}`)
   }
 
   async getPatient(patientId: string): Promise<Patient> {
-    let currentUser = await this.userApi.getCurrentUser();
-    let foundPatient = await this.patientApi.getPatientWithUser(currentUser, patientId);
+    let currentUser = await this.userApi.getCurrentUser()
+    let foundPatient = await this.patientApi.getPatientWithUser(currentUser, patientId)
 
     if (foundPatient) {
-      return PatientMapper.toPatient(foundPatient)!;
+      return PatientMapper.toPatient(foundPatient)!
     }
 
     throw Error(`Could not find patient ${patientId} with user ${currentUser.id}`)
   }
 
   async filterPatients(filter: Filter<Patient>, nextPatientId?: string, limit?: number): Promise<PaginatedListPatient> {
-    return PaginatedListMapper.toPaginatedListPatient(await this.patientApi.filterPatientsBy(undefined, nextPatientId, limit, undefined, undefined, undefined, new FilterChainPatient({
-      filter: FilterMapper.toAbstractFilterDto<Patient>(filter, 'Patient')
-    })))!
+    return PaginatedListMapper.toPaginatedListPatient(
+      await this.patientApi.filterPatientsBy(
+        undefined,
+        nextPatientId,
+        limit,
+        undefined,
+        undefined,
+        undefined,
+        new FilterChainPatient({
+          filter: FilterMapper.toAbstractFilterDto<Patient>(filter, 'Patient'),
+        })
+      )
+    )!
   }
 
   async matchPatients(filter: Filter<Patient>): Promise<Array<string>> {
-    return await this.patientApi.matchPatientsBy(FilterMapper.toAbstractFilterDto<Patient>(filter, 'Patient'));
+    return await this.patientApi.matchPatientsBy(FilterMapper.toAbstractFilterDto<Patient>(filter, 'Patient'))
   }
 
   async giveAccessTo(patient: Patient, delegatedTo: string): Promise<Patient> {
@@ -89,16 +100,18 @@ export class PatientApiImpl implements PatientApi {
     const dataOwnerId = this.userApi.getDataOwnerOf(currentUser)
     const patientToModify = PatientMapper.toPatientDto(patient)!
 
-    if (patientToModify.delegations == undefined || patientToModify.delegations[delegatedTo].length == 0) {
+    if (patientToModify.delegations == undefined || patientToModify.delegations[dataOwnerId].length == 0) {
       throw Error(`User ${currentUser.id} may not access patient information`)
     }
 
-    return await this.cryptoApi.extractPreferredSfk(patientToModify, dataOwnerId, true)
-      .then((secretKey) => {
-        if (!secretKey) {
-          throw Error(`User ${currentUser.id} could not decrypt secret info of patient ${patientToModify.id}`)
+    return await this.cryptoApi
+      .extractDelegationsSFKs(patientToModify, dataOwnerId)
+      .then((delKeys) => {
+        if (delKeys.extractedKeys.length == 0) {
+          throw Error(`User ${currentUser.id} could not decrypt secret info of patient ${patient.id}`)
         }
-        return secretKey
+
+        return delKeys.extractedKeys.shift()!
       })
       .then((secretKey) => this.cryptoApi.addDelegationsAndEncryptionKeys(null, patientToModify, dataOwnerId, delegatedTo, secretKey, null))
       .then((patientWithNewDelegations) => this.cryptoApi.extractEncryptionsSKs(patientWithNewDelegations, dataOwnerId))
@@ -110,18 +123,38 @@ export class PatientApiImpl implements PatientApi {
         return encKeys.extractedKeys.shift()!
       })
       .then((encKey) => this.cryptoApi.addDelegationsAndEncryptionKeys(null, patientToModify, dataOwnerId, delegatedTo, null, encKey))
-      .then((patientWithUpdatedAccesses) => this.patientApi.modifyPatientWithUser(currentUser, patientWithUpdatedAccesses))
+      .then(async (patientWithUpdatedAccesses) => {
+        const currentPatient = await this.patientApi.getPatientWithUser(currentUser, patientWithUpdatedAccesses.id!)
+        const patientToUpdate = new PatientDto({...currentPatient,
+          delegations: patientWithUpdatedAccesses.delegations,
+          encryptionKeys: patientWithUpdatedAccesses.encryptionKeys
+        })
+        return this.patientApi.modifyPatientWithUser(currentUser, patientToUpdate);
+      })
       .then((updatedPatient) => {
         if (!updatedPatient) {
-          throw Error(`Impossible to givve access to ${delegatedTo} to patient ${patientToModify.id} information`)
+          throw Error(`Impossible to give access to ${delegatedTo} to patient ${patientToModify.id} information`)
         }
 
         return PatientMapper.toPatient(updatedPatient)!
       })
   }
 
-  async subscribeToPatientEvents(eventTypes: ("CREATE" | "UPDATE" | "DELETE")[], filter: Filter<Patient> | undefined, eventFired: (patient: Patient) => Promise<void>): Promise<Connection> {
-    let currentUser = await this.userApi.getCurrentUser();
-    return await subscribeToEntityEvents(this.basePath, this.username!, this.password!, "Patient", eventTypes, filter, eventFired, async encrypted => (await this.patientApi.decrypt(currentUser, [encrypted]))[0]).then((rs) => new ConnectionImpl(rs))
+  async subscribeToPatientEvents(
+    eventTypes: ('CREATE' | 'UPDATE' | 'DELETE')[],
+    filter: Filter<Patient> | undefined,
+    eventFired: (patient: Patient) => Promise<void>
+  ): Promise<Connection> {
+    let currentUser = await this.userApi.getCurrentUser()
+    return await subscribeToEntityEvents(
+      this.basePath,
+      this.username!,
+      this.password!,
+      'Patient',
+      eventTypes,
+      filter,
+      eventFired,
+      async (encrypted) => (await this.patientApi.decrypt(currentUser, [encrypted]))[0]
+    ).then((rs) => new ConnectionImpl(rs))
   }
 }
--- test/apis/patient-api.ts
+++ test/apis/patient-api.ts
@@ -1,46 +1,46 @@
-import "mocha";
-import {medTechApi} from "../../src/apis/medTechApi";
-import "isomorphic-fetch";
-import {webcrypto} from "crypto";
+import 'mocha';
+import 'isomorphic-fetch';
 
-import {hex2ua} from "@icure/api";
-
-import {LocalStorage} from "node-localstorage";
-import * as os from "os";
-import {assert} from "chai";
-import {Patient} from "../../src/models/Patient";
-import {HealthcareElement} from "../../src/models/HealthcareElement";
+import {LocalStorage} from 'node-localstorage';
+import * as os from 'os';
+import {assert} from 'chai';
+import {Patient} from '../../src/models/Patient';
+import {HealthcareElement} from '../../src/models/HealthcareElement';
+import {TestUtils} from "../test-utils";
 
 const tmp = os.tmpdir();
-console.log("Saving keys in " + tmp);
+console.log('Saving keys in ' + tmp);
 (global as any).localStorage = new LocalStorage(tmp, 5 * 1024 * 1024 * 1024);
-(global as any).Storage = "";
+(global as any).Storage = '';
 
 const iCureUrl =
-  process.env.ICURE_TS_TEST_URL ?? "https://kraken.icure.dev/rest/v1";
-const userName = process.env.ICURE_TS_TEST_USER!;
-const password = process.env.ICURE_TS_TEST_PWD!;
-const privKey = process.env.ICURE_TS_TEST_PRIV_KEY!;
+  process.env.ICURE_TS_TEST_URL ?? 'https://kraken.icure.dev/rest/v1';
+const msgGtwUrl =
+  process.env.ICURE_TS_TEST_MSG_GTW_URL ?? "https://msg-gw.icure.cloud/ic";
+const patAuthProcessId =
+  process.env.ICURE_TS_TEST_PAT_AUTH_PROCESS_ID ??
+  "6a355458dbfa392cb5624403190c39e5";
+const authProcessHcpId = process.env.ICURE_TS_TEST_AUTH_PROCESS_HCP_ID!;
+const hcpUserName = process.env.ICURE_TS_TEST_HCP_USER!;
+const hcpPassword = process.env.ICURE_TS_TEST_HCP_PWD!;
+const hcpPrivKey = process.env.ICURE_TS_TEST_HCP_PRIV_KEY!;
+const patUserName = process.env.ICURE_TS_TEST_PAT_USER!;
+const patPassword = process.env.ICURE_TS_TEST_PAT_PWD!;
+const patPrivKey = process.env.ICURE_TS_TEST_PAT_PRIV_KEY!;
+const hcp2UserName = process.env.ICURE_TS_TEST_HCP_2_USER!;
+const hcp2Password = process.env.ICURE_TS_TEST_HCP_2_PWD!;
+const hcp2PrivKey = process.env.ICURE_TS_TEST_HCP_2_PRIV_KEY!;
 
-describe("Patient API", () => {
-  it("Can create a patient and a related Healthcare Element", async () => {
-    const medtechApi = medTechApi()
-      .withICureBasePath(iCureUrl)
-      .withUserName(userName)
-      .withPassword(password)
-      .withCrypto(webcrypto as any)
-      .build();
-
-    const loggedUser = await medtechApi.userApi.getLoggedUser();
-    await medtechApi.cryptoApi.loadKeyPairsAsTextInBrowserLocalStorage(
-      loggedUser.healthcarePartyId!,
-      hex2ua(privKey)
-    );
+describe('Patient API', () => {
+  it('Can create a patient and a related Healthcare Element', async () => {
+    const apiAndUser = await TestUtils.createMedTechApiAndLoggedUserFor(iCureUrl, hcpUserName, hcpPassword, hcpPrivKey)
+    const medtechApi = apiAndUser.api;
+    const loggedUser = apiAndUser.user;
 
     const patientToCreate = new Patient({
-      firstName: "John",
-      lastName: "Snow",
-      note: "Winter is coming",
+      firstName: 'John',
+      lastName: 'Snow',
+      note: 'Winter is coming',
     });
 
     // when
@@ -50,9 +50,9 @@ describe("Patient API", () => {
 
     // Then
     assert(patient.id != undefined);
-    assert(patient.firstName == "John");
-    assert(patient.lastName == "Snow");
-    assert(patient.note == "Winter is coming");
+    assert(patient.firstName == 'John');
+    assert(patient.lastName == 'Snow');
+    assert(patient.note == 'Winter is coming');
     assert(
       patient.systemMetaData?.delegations[loggedUser.healthcarePartyId!] !=
         undefined
@@ -64,7 +64,7 @@ describe("Patient API", () => {
 
     // Init
     const healthElementToCreate = new HealthcareElement({
-      note: "Hero Syndrome",
+      note: 'Hero Syndrome',
     });
 
     const createdHealthElement =
@@ -75,12 +75,76 @@ describe("Patient API", () => {
 
     // Then
     assert(createdHealthElement.id != undefined);
-    assert(createdHealthElement.note == "Hero Syndrome");
+    assert(createdHealthElement.note == 'Hero Syndrome');
     assert(createdHealthElement.systemMetaData?.secretForeignKeys != undefined);
     assert(
       createdHealthElement.systemMetaData?.delegations[
         loggedUser.healthcarePartyId!
       ] != undefined
     );
+  });
+
+  it('Patient sharing its own information with HCP', async () => {
+    const patApiAndUser = await TestUtils.signUpUserUsingEmail(iCureUrl, msgGtwUrl, patAuthProcessId, authProcessHcpId)
+    const patApi = patApiAndUser.api;
+    const patUser = patApiAndUser.user;
+
+    const hcpApiAndUser = await TestUtils.createMedTechApiAndLoggedUserFor(iCureUrl, hcp2UserName, hcp2Password, hcp2PrivKey)
+    const hcpApi = hcpApiAndUser.api;
+    const hcpUser = hcpApiAndUser.user;
+    const currentHcp = await hcpApi.healthcareProfessionalApi.getHealthcareProfessional(hcpUser.healthcarePartyId!);
+
+    const currentPatient = await patApi.patientApi.getPatient(patUser.patientId!);
+    const sharedPatient = await patApi.patientApi.giveAccessTo(currentPatient, currentHcp.id!);
+
+    assert(sharedPatient.systemMetaData!.delegations[currentHcp.id!] != undefined);
+    assert(sharedPatient.systemMetaData!.encryptionKeys[currentHcp.id!] != undefined);
+
+    const hcpPatient = await hcpApi.patientApi.getPatient(sharedPatient.id!);
+    assert(hcpPatient != null);
+    assert(hcpPatient.id == sharedPatient.id);
+  }).timeout(60000);
+
+  it('Patient may not access info of another patient', async () => {
+    const hcpApiAndUser = await TestUtils.createMedTechApiAndLoggedUserFor(iCureUrl, hcpUserName, hcpPassword, hcpPrivKey)
+    const hcpApi = hcpApiAndUser.api;
+
+    const patApiAndUser = await TestUtils.createMedTechApiAndLoggedUserFor(iCureUrl, patUserName, patPassword, patPrivKey)
+    const patApi = patApiAndUser.api;
+    const patUser = patApiAndUser.user;
+    const currentPatient = await patApi.patientApi.getPatient(patUser.patientId!);
+
+    const createdPatient = await TestUtils.createDefaultPatient(hcpApi);
+    const sharedPatient = await hcpApi.patientApi.giveAccessTo(createdPatient, currentPatient.id!);
+
+    assert(sharedPatient.systemMetaData!.delegations[currentPatient.id!] != undefined);
+    assert(sharedPatient.systemMetaData!.encryptionKeys[currentPatient.id!] != undefined);
+
+    await patApi.patientApi.getPatient(sharedPatient.id!)
+      .then(
+        () => {
+          throw Error(`Patient ${currentPatient.id} should not be able to access info of another patient !!`)
+        }, 
+        (e) => assert(e != undefined)
+      );
+  });
+
+  it('HCP sharing healthcare element with another HCP', async () => {
+    const hcp1ApiAndUser = await TestUtils.createMedTechApiAndLoggedUserFor(iCureUrl, hcpUserName, hcpPassword, hcpPrivKey)
+    const hcp1Api = hcp1ApiAndUser.api;
+
+    const hcp2ApiAndUser = await TestUtils.createMedTechApiAndLoggedUserFor(iCureUrl, hcp2UserName, hcp2Password, hcp2PrivKey)
+    const hcp2Api = hcp2ApiAndUser.api;
+    const hcp2User = hcp2ApiAndUser.user;
+
+    const createdPatient = await TestUtils.createDefaultPatient(hcp1Api);
+    const sharedPatient = await hcp1Api.patientApi.giveAccessTo(createdPatient, hcp2User.healthcarePartyId!);
+
+    assert(sharedPatient.systemMetaData!.delegations[hcp2User.healthcarePartyId!] != undefined);
+    assert(sharedPatient.systemMetaData!.encryptionKeys[hcp2User.healthcarePartyId!] != undefined);
+
+    const hcp2Patient = await hcp2Api.patientApi.getPatient(sharedPatient.id!);
+    assert(hcp2Patient != null);
+    assert(hcp2Patient.id == sharedPatient.id);
   });
 });
