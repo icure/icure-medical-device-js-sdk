7daa01bb9652ccd4147f108fdb4eca5db2e28280
Vincenzo Claudio Pierro <v.c.pierro@gmail.com>
2022-08-03 12:37:31 +0200

    MDT-69 #comment subtask 1, added Notification model and mapper to and from MaintenanceTask #time 5h

---
+++ src/models/Notification.ts
@@ -0,0 +1,77 @@
+import {Identifier} from "./Identifier";
+import {Property} from "./Property";
+import {SystemMetaDataEncrypted} from "./SystemMetaDataEncrypted";
+
+export class Notification {
+  constructor(json: INotification) {
+    Object.assign(this as Notification, json);
+  }
+
+  /**
+   * The Id of the notification. We encourage using either a v4 UUID or a HL7 Id.
+   */
+  id?: string;
+  /**
+   * The status of the notification.
+   */
+  status?: maintenanceTaskStatusEnum;
+  /**
+   * The creation date of the notification (encoded as epoch).
+   */
+  created?: number;
+  /**
+   * Soft delete (unix epoch in ms) timestamp of the patient
+   */
+  endOfLife?: number;
+  /**
+   * the soft delete timestamp. When a patient is ”deleted“, this is set to a non null value: the moment of the deletion
+   */
+  deletionDate?: number;
+  /**
+   * Typically used for business / client identifiers. An identifier should identify a notification uniquely and unambiguously. However, iCure can't guarantee the uniqueness of those identifiers : This is something you need to take care of.
+   */
+  identifier?: Array<Identifier>;
+  /**
+   * the last modification date of the notification (encoded as epoch).
+   */
+  modified?: number;
+  /**
+   * The id of the [User] that created this notification. When creating the notification, this field will be filled automatically by the current user id if not provided.
+   */
+  author?: string;
+  /**
+   * The id of the data owner that is responsible for this notification. When creating the notification, will be filled automatically by the current user data owner id ([HealthcareProfessional], [Patient] or [MedicalDevice]) if missing
+   */
+  responsible?: string;
+  /**
+   * Additional properties for the notification.
+   */
+  properties?: Array<Property>;
+  /**
+   * The type of the notification.
+   */
+  type?: notificationTypeEnum;
+  systemMetadata?: SystemMetaDataEncrypted;
+}
+
+export interface INotification {
+  id?: string;
+  status?: maintenanceTaskStatusEnum;
+  identifier?: Array<Identifier>;
+  created?: number;
+  modified?: number;
+  endOfLife?: number;
+  deletionDate?: number;
+  author?: string;
+  responsible?: string;
+  properties?: Array<Property>;
+  type?: notificationTypeEnum;
+  systemMetadata?: SystemMetaDataEncrypted;
+}
+
+export enum notificationTypeEnum {
+  KEY_PAIR_UPDATE = "KEY_PAIR_UPDATE",
+  NEW_USER_OWN_DATA_ACCESS = "NEW_USER_OWN_DATA_ACCESS",
+  OTHER = "OTHER"
+}
+export type maintenanceTaskStatusEnum = "pending" | "ongoing" | "cancelled" | "completed";
---
+++ test/models/notification.ts
@@ -0,0 +1,34 @@
+import 'mocha';
+import {Notification, notificationTypeEnum} from '../../src/models/Notification';
+import {v4 as uuid} from 'uuid';
+import {MaintenanceTask} from "@icure/api/icc-api/model/MaintenanceTask";
+import {Delegation, Property, SystemMetaDataEncrypted} from "../../dist";
+import {assert} from "chai";
+
+describe('Notification model test', () => {
+  it('Instantiation of Notification model - Success', () => {
+    const newNotification = new Notification({
+      id: uuid(),
+      status: MaintenanceTask.StatusEnum.Pending,
+      identifier: [],
+      created: new Date().getTime(),
+      modified: new Date().getTime(),
+      deletionDate: new Date().getTime(),
+      endOfLife: new Date().getTime(),
+      author: uuid(),
+      responsible: uuid(),
+      properties: [new Property({id: uuid()})],
+      type: notificationTypeEnum.OTHER,
+      systemMetadata: new SystemMetaDataEncrypted({
+        delegations: { 'TEST_ID': new Set([new Delegation({owner: uuid(), delegatedTo: uuid()})]) },
+        encryptionKeys: { 'TEST_ID': new Set([new Delegation({owner: uuid(), delegatedTo: uuid()})]) }
+      })
+    });
+    assert(newNotification);
+  });
+
+  it('All the fields can be null', () => {
+    const newNotification = new Notification({});
+    assert(newNotification);
+  });
+});
