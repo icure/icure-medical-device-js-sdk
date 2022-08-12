b123995f70afa166e5d085303a0bc8ad9844bb7e
Vincenzo Claudio Pierro <v.c.pierro@gmail.com>
2022-08-12 17:31:20 +0200

    MDT-96 #comment finished implementation of createAndInviteUser

--- src/apis/MessageGatewayApi.ts
+++ src/apis/MessageGatewayApi.ts
@@ -1,12 +1,11 @@
-import {EmailMessageFactory, SMSMessageFactory} from "../utils/gatewayMessageFactory";
 import {XHR} from "@icure/api";
 import {EmailMessage, SMSMessage} from "../utils/gatewayMessageFactory";
 
 export interface MessageGatewayApi {
 
-  sendEmail(recipientEmail: string, emailFactory: EmailMessageFactory): Promise<XHR.Data | null>;
+  sendEmail(recipientEmail: string, email: EmailMessage): Promise<XHR.Data | null>;
 
-  sendSMS(recipientMobileNumber: string, smsFactory: SMSMessageFactory): Promise<XHR.Data | null>;
+  sendSMS(recipientMobileNumber: string, sms: SMSMessage): Promise<XHR.Data | null>;
 
   startAuthenticationProcess(requestId: string, healthcareProfessionalId: string | undefined, firstName: string, lastName: string, email: string, recaptcha: string, mobilePhone?: string): Promise<XHR.Data | null>;
 
