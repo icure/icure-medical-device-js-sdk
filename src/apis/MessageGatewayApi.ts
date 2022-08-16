15d997c927be685e307d47f5840f5234197601ed
Vincenzo Claudio Pierro <v.c.pierro@gmail.com>
2022-08-16 14:56:16 +0200

    MDT-96 #comment added tests, small modifications

--- src/apis/MessageGatewayApi.ts
+++ src/apis/MessageGatewayApi.ts
@@ -3,9 +3,9 @@ import {EmailMessage, SMSMessage} from "../utils/gatewayMessageFactory";
 
 export interface MessageGatewayApi {
 
-  sendEmail(recipientEmail: string, email: EmailMessage): Promise<XHR.Data | null>;
+  sendEmail(recipientEmail: string, email: EmailMessage, anonymous: boolean): Promise<XHR.Data | null>;
 
-  sendSMS(recipientMobileNumber: string, sms: SMSMessage): Promise<XHR.Data | null>;
+  sendSMS(recipientMobileNumber: string, sms: SMSMessage, anonymous: boolean): Promise<XHR.Data | null>;
 
   startAuthenticationProcess(requestId: string, healthcareProfessionalId: string | undefined, firstName: string, lastName: string, email: string, recaptcha: string, mobilePhone?: string): Promise<XHR.Data | null>;
 
