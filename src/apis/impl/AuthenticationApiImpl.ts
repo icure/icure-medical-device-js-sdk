23a07d47b6043d360e2e1e0285698cebeedbf12a
Lisa Trapletti <lt@taktik.com>
2022-09-09 09:29:52 +0200

    MDT-173 Fix AuthenticationProcess instantiation in startAuthentication

--- src/apis/impl/AuthenticationApiImpl.ts
+++ src/apis/impl/AuthenticationApiImpl.ts
@@ -60,7 +60,7 @@ export class AuthenticationApiImpl implements AuthenticationApi {
       "text/plain")
 
     if (res.statusCode < 400) {
-      return new AuthenticationProcess({requestId, login: email ?? mobilePhone, bypassTokenCheck});
+      return new AuthenticationProcess({requestId, login: (email ?? mobilePhone)!, bypassTokenCheck});
     }
 
     return null;
