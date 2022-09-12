a1f1887fe5312fe26b0cd47b6dc6cab3b4758ac4
Clément VANDENDAELEN <cv@icure.com>
2022-09-12 09:18:48 +0200

    MDT-77 Fixed tests after merge

--- test/apis/user-api.ts
+++ test/apis/user-api.ts
@@ -1,4 +1,4 @@
-import {TestUtils} from "../test-utils";
+import {getEnvVariables, TestUtils} from "../test-utils";
 import {v4 as uuid} from 'uuid';
 import {assert} from "chai";
 import 'isomorphic-fetch'
@@ -11,17 +11,17 @@ import {TextDecoder, TextEncoder} from "util";
 ;(global as any).TextDecoder = TextDecoder
 ;(global as any).TextEncoder = TextEncoder
 
-const iCureUrl =
-  process.env.ICURE_TS_TEST_URL ?? "https://kraken.icure.dev/rest/v1";
-const msgGtwUrl =
-  process.env.ICURE_TS_TEST_MSG_GTW_URL ?? "https://msg-gw.icure.cloud/ic";
-const authProcessHcpId = process.env.ICURE_TS_TEST_AUTH_PROCESS_HCP_ID!;
-const patAuthProcessId = process.env.ICURE_TS_TEST_PAT_AUTH_PROCESS_ID ?? "6a355458dbfa392cb5624403190c39e5"; // pragma: allowlist secret
+const {iCureUrl: iCureUrl, msgGtwUrl: msgGtwUrl, authProcessHcpId: authProcessHcpId, specId: specId} = getEnvVariables()
 
 describe('User API', () => {
   it('Management of auto-delegations', async () => {
+    const patAuthProcessId = process.env.ICURE_TS_TEST_PAT_AUTH_PROCESS_ID ?? "6a355458dbfa392cb5624403190c39e5";
+
     try {
-      const {api, user} = await TestUtils.signUpUserUsingEmail(iCureUrl, msgGtwUrl, patAuthProcessId, authProcessHcpId);
+      const {
+        api,
+        user
+      } = await TestUtils.signUpUserUsingEmail(iCureUrl, msgGtwUrl, specId, patAuthProcessId, authProcessHcpId);
 
       const currentUser = await api.userApi.getLoggedUser()
 
