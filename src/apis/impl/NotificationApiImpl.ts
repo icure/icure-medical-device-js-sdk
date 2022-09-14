3f36dbe61c209a7eec0161acde4a7bd098eaa53c
Clément VANDENDAELEN <cv@icure.com>
2022-09-14 11:33:08 +0200

    Added Subscription API to Notification and HealthcareElement

--- src/apis/impl/NotificationApiImpl.ts
+++ src/apis/impl/NotificationApiImpl.ts
@@ -1,10 +1,6 @@
 import {NotificationApi} from "../NotificationApi";
 import {Notification} from "../../models/Notification";
-import {
-  FilterChainMaintenanceTask,
-  IccHcpartyXApi,
-  IccUserXApi, MaintenanceTask, User
-} from "@icure/api";
+import {FilterChainMaintenanceTask, IccHcpartyXApi, IccUserXApi, MaintenanceTask, User} from "@icure/api";
 import {IccMaintenanceTaskXApi} from "@icure/api/icc-x-api/icc-maintenance-task-x-api";
 import {NotificationMapper} from "../../mappers/notification";
 import {PaginatedListNotification} from "../../models/PaginatedListNotification";
@@ -12,6 +8,8 @@ import {Filter} from "../../filter/Filter";
 import {PaginatedListMapper} from "../../mappers/paginatedList";
 import {FilterMapper} from "../../mappers/filter";
 import {systemMetaDataEncryptedEquality} from "../../utils/equality";
+import {Connection, ConnectionImpl} from "../../models/Connection";
+import {subscribeToEntityEvents} from "../../utils/rsocket";
 
 export class NotificationApiImpl implements NotificationApi {
 
@@ -19,19 +17,34 @@ export class NotificationApiImpl implements NotificationApi {
   private readonly maintenanceTaskApi: IccMaintenanceTaskXApi;
   private readonly hcpApi: IccHcpartyXApi;
 
-  constructor(api: { userApi: IccUserXApi; maintenanceTaskApi: IccMaintenanceTaskXApi, healthcarePartyApi: IccHcpartyXApi}) {
+
+  private readonly basePath: string
+  private readonly username?: string
+  private readonly password?: string
+
+  constructor(
+    api: { userApi: IccUserXApi; maintenanceTaskApi: IccMaintenanceTaskXApi, healthcarePartyApi: IccHcpartyXApi },
+    basePath: string,
+    username: string | undefined,
+    password: string | undefined
+  ) {
+    this.basePath = basePath
+    this.username = username
+    this.password = password
     this.userApi = api.userApi;
     this.maintenanceTaskApi = api.maintenanceTaskApi;
     this.hcpApi = api.healthcarePartyApi;
   }
 
-  private async createNotification(notification: Notification, user: User, delegate?: string): Promise<any> {
-    if(!delegate) throw new Error("No delegate provided for Notification creation");
-    const inputMaintenanceTask = NotificationMapper.toMaintenanceTaskDto(notification);
-    return this.maintenanceTaskApi.newInstance(user, inputMaintenanceTask, [delegate])
-      .then( task => {
-        return this.maintenanceTaskApi.createMaintenanceTaskWithUser(user, task);
+  async createOrModifyNotification(notification: Notification, delegate?: string): Promise<Notification | undefined> {
+    return this.userApi.getCurrentUser().then(user => {
+      if (!user) throw new Error("There is no user currently logged in");
+      const notificationPromise = !notification?.rev ? this.createNotification(notification, user, delegate)
+        : this.updateNotification(notification, user);
+      return notificationPromise.then((createdTask) => {
+        return NotificationMapper.toNotification(createdTask as MaintenanceTask);
       });
+    });
   }
 
   private async updateNotification(notification: Notification, user: User): Promise<any> {
@@ -51,20 +64,9 @@ export class NotificationApiImpl implements NotificationApi {
     return this.maintenanceTaskApi.modifyMaintenanceTaskWithUser(user, inputMaintenanceTask);
   }
 
-  async createOrModifyNotification(notification: Notification, delegate?: string): Promise<Notification | undefined> {
-    return this.userApi.getCurrentUser().then( user => {
-      if(!user) throw new Error("There is no user currently logged in");
-      const notificationPromise = !notification?.rev ? this.createNotification(notification, user, delegate)
-        : this.updateNotification(notification, user);
-      return notificationPromise.then( (createdTask) => {
-        return NotificationMapper.toNotification(createdTask as MaintenanceTask);
-      });
-    });
-  }
-
   async deleteNotification(notificationId: string): Promise<string | undefined> {
-    return this.userApi.getCurrentUser().then( user => {
-      if(!user) throw new Error("There is no user currently logged in");
+    return this.userApi.getCurrentUser().then(user => {
+      if (!user) throw new Error("There is no user currently logged in");
       return this.maintenanceTaskApi.deleteMaintenanceTaskWithUser(user, notificationId).then(identifiers => {
         if (!identifiers || identifiers.length == 0) return undefined;
         return identifiers[0].id;
@@ -73,7 +75,7 @@ export class NotificationApiImpl implements NotificationApi {
   }
 
   async filterNotifications(filter: Filter<Notification>, nextNotificationId?: string, limit?: number): Promise<PaginatedListNotification> {
-    return this.userApi.getCurrentUser().then( user => {
+    return this.userApi.getCurrentUser().then(user => {
       if (!user) throw new Error("There is no user currently logged in");
       return this.maintenanceTaskApi.filterMaintenanceTasksByWithUser(
         user,
@@ -82,19 +84,46 @@ export class NotificationApiImpl implements NotificationApi {
         new FilterChainMaintenanceTask({
           filter: FilterMapper.toAbstractFilterDto(filter, 'Notification')
         })
-      ).then( paginatedList => {
+      ).then(paginatedList => {
         return PaginatedListMapper.toPaginatedListNotification(paginatedList)!
       });
     });
   }
 
   async getNotification(notificationId: string): Promise<Notification | undefined> {
-    return this.userApi.getCurrentUser().then( user => {
-      if(!user) throw new Error("There is no user currently logged in");
-      return this.maintenanceTaskApi.getMaintenanceTaskWithUser(user, notificationId).then( task => {
+    return this.userApi.getCurrentUser().then(user => {
+      if (!user) throw new Error("There is no user currently logged in");
+      return this.maintenanceTaskApi.getMaintenanceTaskWithUser(user, notificationId).then(task => {
         return NotificationMapper.toNotification(task)
       });
     });
+  }
+
+  async subscribeToNotificationEvents(
+    eventTypes: ("CREATE" | "UPDATE" | "DELETE")[],
+    filter: Filter<Notification>,
+    eventFired: (dataSample: Notification) => Promise<void>,
+    options: { keepAlive?: number; lifetime?: number; connectionMaxRetry?: number; connectionRetryIntervalMs?: number } = {}
+  ): Promise<Connection> {
+    return subscribeToEntityEvents(
+      this.basePath,
+      this.username!,
+      this.password!,
+      'Notification',
+      eventTypes,
+      filter,
+      eventFired,
+      options,
+    ).then((rs) => new ConnectionImpl(rs))
+  }
+
+  private async createNotification(notification: Notification, user: User, delegate?: string): Promise<any> {
+    if (!delegate) throw new Error("No delegate provided for Notification creation");
+    const inputMaintenanceTask = NotificationMapper.toMaintenanceTaskDto(notification);
+    return this.maintenanceTaskApi.newInstance(user, inputMaintenanceTask, [delegate])
+      .then(task => {
+        return this.maintenanceTaskApi.createMaintenanceTaskWithUser(user, task);
+      });
   }
 
 }
