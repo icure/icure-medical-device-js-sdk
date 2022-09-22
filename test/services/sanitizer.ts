efc0c1bd96bb9a20600326455483d4cd4da6b905
Clément VANDENDAELEN <cv@icure.com>
2022-09-22 16:06:06 +0200

    MDT-75 Fixed sanitizer tests

--- test/services/sanitizer.ts
+++ test/services/sanitizer.ts
@@ -6,13 +6,19 @@ describe('Sanitizer', () => {
   const sanitizer = new SanitizerImpl();
 
   it('should validate email', () => {
-    assert(sanitizer.validateEmail('test@icure.com') != null);
-    assert(sanitizer.validateEmail('test+123@icure.com') != null);
-    assert(sanitizer.validateEmail('Test@icure.com') == null);
-    assert(sanitizer.validateEmail('johnwick') == null);
+    assert(sanitizer.validateEmail('test@icure.com') != null, 'Email should be valid');
+    assert(sanitizer.validateEmail('test+123@icure.com') != null, 'Email should be valid');
+    assert(sanitizer.validateEmail('Test@icure.com') != null, 'Email with caps should be valid, email is case insensitive');
+    assert(sanitizer.validateEmail('johnwick') == null, 'Email without @ and provider should be invalid');
+    assert(sanitizer.validateEmail('johnwick@') == null, 'Email without provider should be invalid');
+    assert(sanitizer.validateEmail('johnwick@thecontinental') == null, 'Email without [.xxx] should be invalid');
+    assert(sanitizer.validateEmail('johnwick@thecontinental.') == null, 'Email without .[xxx] should be invalid');
   });
   it('should validate mobile phone', () => {
-    assert(sanitizer.validateMobilePhone('0470123456') == null);
-    assert(sanitizer.validateMobilePhone('+32470123456') != null);
+    assert(sanitizer.validateMobilePhone('0470123456') == null, 'Mobile phone without national identifier should be invalid');
+    assert(sanitizer.validateMobilePhone('+32470123456') != null, 'Mobile phone with national identifier should be valid');
+    assert(sanitizer.validateMobilePhone('+324701234567890') != null, 'Mobile phone with national identifier and up to 15 digits should be valid');
+    assert(sanitizer.validateMobilePhone('+3247012345678901') == null, 'Mobile phone with national identifier and more than 15 digits should be invalid');
+    assert(sanitizer.validateMobilePhone('+32470123456789a') == null, 'Mobile phone with national identifier and non numeric characters should be invalid');
   });
 });
