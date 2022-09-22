69891b6535a8048ae44078c8082b914a8c02fe1c
Clément VANDENDAELEN <cv@icure.com>
2022-09-22 15:30:22 +0200

    MDT-75 Implemented error handling on MessageGatewayApi

--- src/apis/impl/MessageGatewayApiImpl.ts
+++ src/apis/impl/MessageGatewayApiImpl.ts
@@ -6,9 +6,20 @@ import Header = XHR.Header;
 
 export class MessageGatewayApiImpl implements MessageGatewayApi {
 
+  private readonly errorHandler: ErrorHandler;
+
+  private readonly fetchImpl?: (input: RequestInfo, init?: RequestInit) => Promise<Response>;
+  private readonly msgGtwUrl: string;
+  private readonly specId: string;
+  private readonly authHeader: XHR.Header | null;
+  private readonly headers: Header[];
+  private readonly sanitizer: Sanitizer;
+
   constructor(
     msgGtwUrl: string,
     specId: string,
+    errorHandler: ErrorHandler,
+    sanitizer: Sanitizer,
     username?: string,
     password?: string,
     fetchImpl: (input: RequestInfo, init?: RequestInit) => Promise<Response> = typeof window !== 'undefined'
@@ -22,48 +33,60 @@ export class MessageGatewayApiImpl implements MessageGatewayApi {
     this.specId = specId;
     this.authHeader = (!!username && !!password) ? new Header(
       "Authorization",
-      "Basic "+Buffer.from(`${username}:${password}`).toString('base64')
+      "Basic " + Buffer.from(`${username}:${password}`).toString('base64')
     ) : null;
-    this.headers = [new Header("Content-Type", "application/json")]
+    this.headers = [new Header("Content-Type", "application/json")];
+    this.errorHandler = errorHandler;
+    this.sanitizer = sanitizer;
   }
-
-  private readonly fetchImpl?: (input: RequestInfo, init?: RequestInit) => Promise<Response>;
-  private readonly msgGtwUrl: string;
-  private readonly specId: string;
-  private readonly authHeader: XHR.Header | null;
-  private readonly headers: Header[];
 
   async sendEmail(recipientEmail: string, email: EmailMessage): Promise<boolean> {
     if (!this.authHeader) return false;
 
+    const emailTo = this.sanitizer.validateEmail(recipientEmail)
+
+    if (!emailTo) {
+      throw this.errorHandler.createErrorWithMessage(`Invalid email address ${recipientEmail}`)
+    }
+
     const res = await XHR.sendCommand('POST',
-      `${this.msgGtwUrl}/${this.specId}/email/to/${recipientEmail}`,
+      `${this.msgGtwUrl}/${this.specId}/email/to/${emailTo}`,
       this.headers.concat([this.authHeader]),
       email,
       this.fetchImpl
-    );
+    ).catch(e => {
+      throw this.errorHandler.createErrorFromAny(e)
+    });
 
     if (res.statusCode < 400) {
       return true;
     } else {
-      throw new Error(`Message Gateway returned error response ${res.statusCode}`);
+      throw this.errorHandler.createErrorWithMessage(`Message Gateway returned error response ${res.statusCode}`)
     }
   }
 
   async sendSMS(recipientMobileNumber: string, sms: SMSMessage): Promise<boolean> {
     if (!this.authHeader) return false;
 
+    const mobileNumber = this.sanitizer.validateMobilePhone(recipientMobileNumber)
+
+    if (!mobileNumber) {
+      throw this.errorHandler.createErrorWithMessage(`Invalid mobile number ${recipientMobileNumber}`)
+    }
+
     const res = await XHR.sendCommand('POST',
-      `${this.msgGtwUrl}/${this.specId}/sms/to/${recipientMobileNumber}`,
+      `${this.msgGtwUrl}/${this.specId}/sms/to/${mobileNumber}`,
       this.headers.concat([this.authHeader]),
       sms,
       this.fetchImpl
-    );
+    ).catch(e => {
+      throw this.errorHandler.createErrorFromAny(e)
+    });
 
     if (res.statusCode < 400) {
       return true;
     } else {
-      throw new Error(`Message Gateway returned error response ${res.statusCode}`);
+      throw this.errorHandler.createErrorWithMessage(`Message Gateway returned error response ${res.statusCode}`)
     }
   }
 
@@ -76,12 +99,14 @@ export class MessageGatewayApiImpl implements MessageGatewayApi {
       processBody,
       this.fetchImpl,
       "text/plain"
-    )
+    ).catch(e => {
+      throw this.errorHandler.createErrorFromAny(e)
+    })
 
     if (res.statusCode < 400) {
       return requestId;
     } else {
-      throw new Error(`Message Gateway returned error response ${res.statusCode}`);
+      throw this.errorHandler.createErrorWithMessage(`Message Gateway returned error response ${res.statusCode}`)
     }
   }
 
@@ -91,12 +116,14 @@ export class MessageGatewayApiImpl implements MessageGatewayApi {
       [],
       undefined,
       this.fetchImpl
-    )
+    ).catch(reason => {
+      throw this.errorHandler.createError(reason)
+    })
 
     if (res.statusCode < 400) {
       return true;
     } else {
-      throw new Error(`Message Gateway returned error response ${res.statusCode}`);
+      throw this.errorHandler.createErrorWithMessage(`Message Gateway returned error response ${res.statusCode}`)
     }
   }
 
