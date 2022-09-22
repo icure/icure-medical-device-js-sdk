ec033202ab17857297395b0f6f20b54e40adce17
Clément VANDENDAELEN <cv@icure.com>
2022-09-22 09:22:30 +0200

    MDT-75 Implemented error handling on HealthcareProfessionalApi

--- src/apis/impl/HealthcareProfessionalApiImpl.ts
+++ src/apis/impl/HealthcareProfessionalApiImpl.ts
@@ -20,43 +20,82 @@ import {firstOrNull} from "../../utils/functionalUtils";
 export class HealthcareProfessionalApiImpl implements HealthcareProfessionalApi {
   private readonly userApi: IccUserXApi;
   private readonly hcpApi: IccHcpartyXApi;
+  private readonly errorHandler: ErrorHandler;
+  private readonly sanitizer: Sanitizer;
 
-  constructor(api: { cryptoApi: IccCryptoXApi; userApi: IccUserXApi; patientApi: IccPatientXApi; contactApi: IccContactXApi; documentApi: IccDocumentXApi; healthcarePartyApi: IccHcpartyXApi, healthcareElementApi: IccHelementXApi}) {
+  constructor(api: { cryptoApi: IccCryptoXApi; userApi: IccUserXApi; patientApi: IccPatientXApi; contactApi: IccContactXApi; documentApi: IccDocumentXApi; healthcarePartyApi: IccHcpartyXApi, healthcareElementApi: IccHelementXApi }, errorHandler: ErrorHandler, sanitizer: Sanitizer) {
     this.userApi = api.userApi;
     this.hcpApi = api.healthcarePartyApi;
+    this.errorHandler = errorHandler;
+    this.sanitizer = sanitizer;
   }
 
   async createOrModifyHealthcareProfessional(healthcareProfessional: HealthcareProfessional): Promise<HealthcareProfessional> {
-    let createdOrModifyHealthcareParty = healthcareProfessional.rev
-      ? await this.hcpApi.modifyHealthcareParty(HealthcareProfessionalMapper.toHealthcarePartyDto(healthcareProfessional))
-      : await this.hcpApi.createHealthcareParty(HealthcareProfessionalMapper.toHealthcarePartyDto(healthcareProfessional))
+    try {
+      const createdOrModifyHealthcareParty = healthcareProfessional.rev
+        ? await this.hcpApi.modifyHealthcareParty(HealthcareProfessionalMapper.toHealthcarePartyDto(healthcareProfessional))
+        : await this.hcpApi.createHealthcareParty(HealthcareProfessionalMapper.toHealthcarePartyDto(healthcareProfessional))
 
-    if (createdOrModifyHealthcareParty) {
-      return HealthcareProfessionalMapper.toHealthcareProfessional(createdOrModifyHealthcareParty)!;
+      if (createdOrModifyHealthcareParty) {
+        return HealthcareProfessionalMapper.toHealthcareProfessional(createdOrModifyHealthcareParty)!;
+      }
+
+      throw this.errorHandler.createErrorWithMessage(`Could not create or modify healthcare professional ${healthcareProfessional.id}`)
+    } catch (e) {
+      if (e instanceof Error) {
+        throw this.errorHandler.createError(e);
+      }
+      throw e;
     }
-
-    throw Error(`Could not create / modify healthcare professional ${healthcareProfessional.id}`)
   }
 
   async deleteHealthcareProfessional(hcpId: string): Promise<string> {
-    const deletedHcpRev = firstOrNull(await this.hcpApi.deleteHealthcareParties(hcpId))?.rev
-    if (deletedHcpRev) {
-      return deletedHcpRev
+    try {
+      const deletedHcpRev = firstOrNull(await this.hcpApi.deleteHealthcareParties(hcpId))?.rev
+      if (deletedHcpRev) {
+        return deletedHcpRev
+      }
+      throw this.errorHandler.createErrorWithMessage(`Could not delete healthcare professional ${hcpId}`)
+    } catch (e) {
+      if (e instanceof Error) {
+        throw this.errorHandler.createError(e);
+      }
+      throw e;
     }
-    throw Error(`Could not delete healthcare professional ${hcpId}`)
   }
 
   async filterHealthcareProfessionalBy(filter: Filter<HealthcareProfessional>, nextHcpId?: string, limit?: number): Promise<PaginatedListHealthcareProfessional> {
-    return PaginatedListMapper.toPaginatedListHealthcareProfessional(await this.hcpApi.filterHealthPartiesBy(nextHcpId, limit, new FilterChainHealthcareParty({
-      filter: FilterMapper.toAbstractFilterDto<HealthcareProfessional>(filter, 'HealthcareProfessional')
-    })))!
+    try {
+      return PaginatedListMapper.toPaginatedListHealthcareProfessional(await this.hcpApi.filterHealthPartiesBy(nextHcpId, limit, new FilterChainHealthcareParty({
+        filter: FilterMapper.toAbstractFilterDto<HealthcareProfessional>(filter, 'HealthcareProfessional')
+      })))!
+    } catch (e) {
+      if (e instanceof Error) {
+        throw this.errorHandler.createError(e);
+      }
+      throw e;
+    }
   }
 
   async getHealthcareProfessional(hcpId: string): Promise<HealthcareProfessional> {
-    return HealthcareProfessionalMapper.toHealthcareProfessional(await this.hcpApi.getHealthcareParty(hcpId, false))
+    try {
+      return HealthcareProfessionalMapper.toHealthcareProfessional(await this.hcpApi.getHealthcareParty(hcpId, false))
+    } catch (e) {
+      if (e instanceof Error) {
+        throw this.errorHandler.createError(e);
+      }
+      throw e;
+    }
   }
 
   async matchHealthcareProfessionalBy(filter: Filter<HealthcareProfessional>): Promise<Array<string>> {
-    return await this.hcpApi.matchHealthcarePartiesBy(FilterMapper.toAbstractFilterDto<HealthcareProfessional>(filter, 'HealthcareProfessional'));
+    try {
+      return await this.hcpApi.matchHealthcarePartiesBy(FilterMapper.toAbstractFilterDto<HealthcareProfessional>(filter, 'HealthcareProfessional'));
+    } catch (e) {
+      if (e instanceof Error) {
+        throw this.errorHandler.createError(e);
+      }
+      throw e;
+    }
   }
 }
