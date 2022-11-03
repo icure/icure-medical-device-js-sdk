175d844e40780de46cd7a7aaa543e0718b933c78
Vincenzo Claudio Pierro <v.c.pierro@gmail.com>
2022-11-03 10:50:26 +0100

    MDT-155 Small fix on equality

--- src/utils/equality.ts
+++ src/utils/equality.ts
@@ -7,8 +7,8 @@ export function deepEquality(val1?: any, val2?: any): boolean {
   else if (xor(!!val1, !!val2)) return false
   else if (Array.isArray(val1) && Array.isArray(val2)) return arrayEquality(val1, val2)
   else if (val1 instanceof Set && val2 instanceof Set) return setEquality(val1, val2)
-  else if ((typeof val1) === "object" && (typeof val2) === "object") return objectEquality(val1, val2)
-  else if ((typeof val1) === (typeof val1)) return val1 === val2
+  else if (typeof val1 === 'object' && typeof val2 === 'object') return objectEquality(val1, val2)
+  else if (typeof val1 === typeof val2) return val1 === val2
   else return false
 }
 
@@ -21,17 +21,18 @@ export function arrayEquality(arr1?: any[], arr2?: any[]): boolean {
   else if (xor(!!arr1, !!arr2)) return false
   else if (!!arr1 && !!arr2 && arr1.length !== arr2.length) return false
 
-  return arr1!.every( (val1, _) =>
-    arr2!.some( (val2) => deepEquality(val1, val2))
-  )
+  return arr1!.every((val1, _) => arr2!.some((val2) => deepEquality(val1, val2)))
 }
 
-export function objectEquality(obj1?: {[key: string]: any}, obj2?: {[key: string]: any}): boolean {
+export function objectEquality(obj1?: { [key: string]: any }, obj2?: { [key: string]: any }): boolean {
   const keys1 = Object.keys(obj1 ?? {})
   const keys2 = Object.keys(obj2 ?? {})
-  return arrayEquality(keys1, keys2) && keys1.every( (key1, _) => {
-    const val1 = obj1!![key1]
-    const val2 = obj2!![key1]
-    return deepEquality(val1, val2)
-  })
+  return (
+    arrayEquality(keys1, keys2) &&
+    keys1.every((key1, _) => {
+      const val1 = obj1!![key1]
+      const val2 = obj2!![key1]
+      return deepEquality(val1, val2)
+    })
+  )
 }
